2d98f0d55feaa5d8b3ff309bb81e461a1a7d1652
Dominic-SR <dominicseduraja3@gmail.com>
2026-04-04 00:43:25 +0530

    create db context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+namespace ASPDotNetCoreWepAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using ASPDotNetCoreWepAPI.Models;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Job> Jobs { get; set; }
+}
